e77816782f2c0ae556bd718332a334a321ee99df
Ben Kilkowski <benkilkowski@outlook.com>
2021-03-26 12:34:35 -0500

    Completed Player Controller

--- Team Unity Project CS4/Assets/Scripts/PlayerMovement.cs
+++ Team Unity Project CS4/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     float horizontalMove = 0f;
-    bool jump = false;
     bool facingRight = true;
 
+    [Header("Forces/Speeds")]
     [SerializeField] float jumpForce = 300f;
     [SerializeField] float moveSpeed = 5f;
+
+    [Header("Ground Checking")]
+    [SerializeField] Transform groundCheck;             //Origin for ground check
+    [SerializeField] LayerMask groundMask;              //Ground Layer
+    bool isGrounded;
+    bool jumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,35 +27,48 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
-        Debug.Log(horizontalMove);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, GetComponent<SpriteRenderer>().bounds.extents.y + 0.1f, groundMask);
+        //Debug.DrawLine(transform.position, transform.position - new Vector3(0, GetComponent<SpriteRenderer>().bounds.extents.y + 0.1f, 0), Color.red);
+        //Debug.Log("Grounded?: " + isGrounded);
+        //Debug.Log("Can Jump?: " + jumping);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded && !jumping)
         {
-            jump = true;
+            jumping = true;
         }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        //Debug.Log(horizontalMove);
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
-        if (jump)
+        if (jumping)
         {
-            rb.AddForce(Vector2.up * jumpForce);
-            jump = false;
+            Jump();
         }
 
         if (horizontalMove < 0.0f && facingRight)
         {
-            facingRight = !facingRight;
-
-            transform.Rotate(0f, 180f, 0f);
+            Flip();
         }
         else if (horizontalMove > 0.0f && !facingRight)
         {
-            facingRight = !facingRight;
-
-            transform.Rotate(0f, 180f, 0f);
+            Flip();
         }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce));
+        jumping = false;
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
     }
 }
